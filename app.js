eebd7458e5da4b4959024d5401575379fd3b44f4
augustin <augustin.tarit@gmail.com>
2023-08-24 18:11:06 +0000

    Tuto n10

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const http = require('http');
 
 const app =express();
 
+app.use((req, res, next) => {
+    console.log('Logging the request');
+    next();
+});
+
+//else
+
+app.use('/users', (req, res, next) => {
+    res.send('This is the users page');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>This is home page</h1>')
+});
+
 app.listen(3000, () => {
     console.log('server started at port 3000');
 })
