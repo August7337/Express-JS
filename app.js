c55dca3d10b7613cc561e2814c5f938a0927ce4c
Your Name <you@example.com>
2023-08-26 19:40:51 +0200

    Tuto n16

--- app.js
+++ app.js
@@ -8,18 +8,26 @@ const homeRoutes = require('./routes/home');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+//Static files
 app.use(express.static(path.join(rootDir, 'public')));
 app.use('/css', express.static(path.join(rootDir, 'node_modules','bootstrap', 'dist', 'css')));
-
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Routes
 app.use(homeRoutes);
 app.use('/users', adminRoutes);
-
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    const viewsData = {
+        pageTitle: 'Page Note Found'
+    };
+    res.status(404).render('404', viewsData);
 });
  
+
+
 app.listen(3000, () => {
     console.log('server started at port 3000');
 });
